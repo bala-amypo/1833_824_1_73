82834558764e4ac2015b794d0fc0d6e0861194e0
bala-amypo <bala@amypo.in>
2025-12-17 10:21:40 +0000

    User commit

--- src/main/java/com/example/demo/model/ApprovalRequest.java
+++ src/main/java/com/example/demo/model/ApprovalRequest.java
@@ -4,13 +4,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import java.time.LocalDateTime;
+
 @Entity
 public class ApprovalRequest{
-    @Id
+  @Id
   @GeneratedValue(strategy=GenerationType.IDENTITY)
-  private long id;
+  private Long id;
   private Long templated;
-   private Long requesterlid;
+  private Long requesterlid;
   private String requestTitle;
   private String requestPayloadJson;
   private String status;
@@ -24,46 +25,49 @@ public class ApprovalRequest{
     public void setId(Long id) {
         this.id = id;
     }
+
     public Long  gettemplated() {
         return templated;
     }
-
     public void settemplated(Long templated) {
         this.templated = templated;
     }
+
     public Long  getrequesterlid() {
         return requesterlid;
     }
-
     public void setrequesterlid(Long requesterlid) {
         this.requesterlid = requesterlid;
     }
+
     public String getrequestTitle() {
         return requestTitle;
     }
     public void setrequestTitle(String requestTitle) {
         this.requestTitle = requestTitle;
     }
+
     public String getrequestPayloadJson() {
         return requestPayloadJson;
     }
     public void setrequestPayloadJson(String requestPayloadJson) {
         this.requestPayloadJson= requestPayloadJson;
     }
+
     public String getstatus() {
         return status;
     }
-
     public void setstatus(String status) {
         this.status= status;
     }
+
     public Integer getcurrentLevel() {
         return currentLevel;
     }
-
     public void setcurrentLevel(Integer currentLevel) {
         this.currentLevel=currentLevel;
     }
+    
      public LocalDateTime getcreatedAt() {
         return createdAt;
     }
