4cf4676cd6612519285d6f86c56fb218d1c8e35d
bala-amypo <bala@amypo.in>
2025-12-17 06:30:04 +0000

    User commit

--- src/main/java/com/example/demo/model/WorkflowTemplate.java
+++ src/main/java/com/example/demo/model/WorkflowTemplate.java
@@ -1,5 +1,49 @@
 public class WorkflowTemplate{
   private long id;
   private String templateName;
+  private String description;
+  private Integer totalLevels;
+  private Boolean active;
+
   
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void settemplateName(String  templatename) {
+        this.templateName = templateName;
+    }
+
+    public void (String email) {
+        this.email = email;
+    }
+
+    public void setPhno(String phno) {
+        this.phno = phno;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhno() {
+        return phno;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
 }
