158f0e241fc31729a825a50d88ab11b3f61cc0c0
bala-amypo <bala@amypo.in>
2025-12-17 08:25:08 +0000

    User commit

--- src/main/java/com/example/demo/model/ApprovalRequest.java
+++ src/main/java/com/example/demo/model/ApprovalRequest.java
@@ -1,3 +1,52 @@
+package com.example.demo.model;
+import.jakarta.persistence.Entity;
+import.jakarta.persistence.Id;
+import.jakarta.persistence.GeneratedValue;
+import.jakarta.persistence.GenerationType;
+
+@Entity
 public class ApprovalRequest{
+    @id
+  @GeneratedValue(GenerationType.IDENTITY)
+  private long id;
+  @Column(unique=true)
+  private Long templated;
+  private Integer levelNumber;
+  private String approverRole;
+  private String instructions;
+
+  
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long  gettemplated() {
+        return templated;
+    }
+
+    public void settemplated(Long templated) {
+        this.templated = templated;
+    }
+    public Integer getlevelNumber() {
+        return levelNumber;
+    }
+    public void setlevelNumber(Integer levelNumber) {
+        this.levelNumber = levelNumber;
+    }
+    public String getapproverRole() {
+        return approverRole;
+    }
+    public void setapproverRole(String approverRole) {
+        this.approverRole= approverRole;
+    }
+    public String getinstructions() {
+        return instructions;
+    }
+
+    public void setactive(String instructions) {
+        this.instructions= instructions;
+    }
 
 }
