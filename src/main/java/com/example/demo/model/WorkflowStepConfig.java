66abf18d0a37fac518b45c62e3fbe1a2ef2cae27
bala-amypo <bala@amypo.in>
2025-12-17 06:50:04 +0000

    User commit

--- src/main/java/com/example/demo/model/WorkflowStepConfig.java
+++ src/main/java/com/example/demo/model/WorkflowStepConfig.java
@@ -1,3 +1,56 @@
+
+package com.example.demo.model;
+import.jakarta.persistence.Entity;
+import.jakarta.persistence.Id;
+import.jakarta.persistence.GeneratedValue;
+import.jakarta.persistence.GenerationType;
+
+
+
+
+@Entity
 public class WorkflowStepConfig{
+  @id
+  @GeneratedValue(GenerationType.IDENTITY)
+  private long id;
+  @Column(unique=true)
+  private Long templated;
+  private String description;
+  private Integer totalLevels;
+  private Boolean active;
+
+  
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String gettemplateName() {
+        return templateName;
+    }
+
+    public void settemplateName(String  templatename) {
+        this.templateName = templateName;
+    }
+    public String getdescription() {
+        return description;
+    }
+    public void setdescription(String discription) {
+        this.description = description;
+    }
+    public Integer gettotalLevels() {
+        return totalLevels;
+    }
+    public void settotalLevels(Integer totalLevels) {
+        this.totalLevels = totalLevels;
+    }
+    public Boolean getactive() {
+        return active;
+    }
+
+    public void setactive(Boolean active) {
+        this.active= active;
+    }
 
 }
--- src/main/java/com/example/demo/model/WorkflowTemplate.java
+++ src/main/java/com/example/demo/model/WorkflowTemplate.java
@@ -3,6 +3,7 @@ import.jakarta.persistence.Entity;
 import.jakarta.persistence.Id;
 import.jakarta.persistence.GeneratedValue;
 import.jakarta.persistence.GenerationType;
+import.jakarta.persistence.unique;
 
 
 
