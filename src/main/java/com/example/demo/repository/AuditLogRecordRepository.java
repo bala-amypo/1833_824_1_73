f0589d4ea98bbc548076b9358970d444476539c6
bala-amypo <bala@amypo.in>
2025-12-18 16:08:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/AuditLogRecordRepository.java
+++ src/main/java/com/example/demo/repository/AuditLogRecordRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.entity.User;
+import com.example.demo.entity.ApprovalActionRepository;
  import java.util.Long;
 
 @Repository
