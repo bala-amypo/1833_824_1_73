f17f125aba7a4b78fe9c73dc7b01a46e1a9279d7
bala-amypo <bala@amypo.in>
2025-12-18 10:12:17 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ApprovalActionService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.ApprovalActionService;
+
+@Service
+public class ApprovalActionService {
+    createStep(WorkflowStepConfig step)
+    
+}
+
+
+
+
+
+
+studentService.java
