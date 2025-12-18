52876a0483fdcfe4b15baeea5768ddb7acd84b7f
bala-amypo <bala@amypo.in>
2025-12-18 15:53:47 +0000

    User commit

--- src/main/java/com/example/demo/service/ApprovalActionService.java
+++ src/main/java/com/example/demo/service/ApprovalActionService.java
@@ -15,6 +15,3 @@ public class ApprovalActionService {
 
 
 
-
-
-studentService.java
---
+++ src/main/java/com/example/demo/service/ApprovalRequestService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.ApprovalActionService;
+
+@Service
+public class ApprovalRequestService {
+    createRequest(ApprovalRequestService req);
+    getRequest(Long templateId);
+    
+}
+
+
+
+
