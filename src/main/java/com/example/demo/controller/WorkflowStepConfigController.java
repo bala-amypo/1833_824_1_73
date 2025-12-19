4190186ee31f6935100d31d6c8e796f9a9d5e928
bala-amypo <bala@amypo.in>
2025-12-19 08:39:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/WorkflowStepConfigController.java
+++ src/main/java/com/example/demo/controller/WorkflowStepConfigController.java
@@ -1,24 +1,24 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.PathVariable;
 
 
-@RestController
-public class  WorkflowStepConfigController{
-    @Autowired
-    private WorkflowStepConfigService abc;
+// @RestController
+// public class  WorkflowStepConfigController{
+//     @Autowired
+//     private WorkflowStepConfigService abc;
 
-    @PostMapping("/api/steps")
-    public WorkflowStepConfig createStep(@RequestBody WorkflowStepConfig xyz) {
-        return abc.save(xyz);
-    }
+//     @PostMapping("/api/steps")
+//     public WorkflowStepConfig createStep(@RequestBody WorkflowStepConfig xyz) {
+//         return abc.save(xyz);
+//     }
    
-    @GetMapping("/template/{templated}")
-    public List<WorkflowStepConfig> getStepsForTemplte(@PathVariable Long templated) {
-        return abc.findByStepsForTemplte(templated);
+//     @GetMapping("/template/{templated}")
+//     public List<WorkflowStepConfig> getStepsForTemplte(@PathVariable Long templated) {
+//         return abc.findByStepsForTemplte(templated);
 
-}
+// }
--- src/main/java/com/example/demo/service/WorkflowTemplateService.java
+++ src/main/java/com/example/demo/service/WorkflowTemplateService.java
@@ -7,11 +7,11 @@ import com.example.demo.entity.WorkflowTemplateService ;
 
 @Service
 public class WorkflowTemplateService  {
-    createTemplate(WorkflowTemplate t)
-    getTemplateById(Long id)
-    getAllTemplates()
-    updateTemplate(Long id,WorkflowTemplat t)
-    activateTemplate(Long id,boolean active)
+    createTemplate(WorkflowTemplate t);
+    getTemplateById(Long id);
+    getAllTemplates();
+    updateTemplate(Long id,WorkflowTemplat t);
+    activateTemplate(Long id,boolean active);
    
 }
 
