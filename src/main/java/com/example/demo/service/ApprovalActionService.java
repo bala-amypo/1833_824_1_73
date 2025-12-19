57e5afde09a2316915ae55609de866a458888cb3
bala-amypo <bala@amypo.in>
2025-12-19 09:51:05 +0000

    User commit

--- src/main/java/com/example/demo/service/ApprovalActionService.java
+++ src/main/java/com/example/demo/service/ApprovalActionService.java
@@ -7,8 +7,7 @@ import com.example.demo.model.ApprovalActionService;
 
 @Service
 public class ApprovalActionService {
-    createStep(WorkflowStepConfig step);
-    getStepsForTemplate(Long templateId);
+    recordAction
     
 }
 
