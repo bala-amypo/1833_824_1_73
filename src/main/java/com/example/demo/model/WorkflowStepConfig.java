2375b970759cc30ff6ec2fddd45c4479232b4ab4
bala-amypo <bala@amypo.in>
2025-12-19 16:03:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/WorkflowStepConfig.java
@@ -0,0 +1,66 @@
+
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+
+
+@Entity
+public class WorkflowStepConfig{
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  private Long id;
+
+  private Long templated;
+  private Integer levelNumber;
+  private String approverRole;
+  private String instructions;
+
+  
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long  gettemplated() {
+        return templated;
+    }
+
+    public void settemplated(Long templated) {
+        this.templated = templated;
+    }
+    public Integer getlevelNumber() {
+        return levelNumber;
+    }
+    public void setlevelNumber(Integer levelNumber) {
+        this.levelNumber = levelNumber;
+    }
+    public String getapproverRole() {
+        return approverRole;
+    }
+    public void setapproverRole(String approverRole) {
+        this.approverRole= approverRole;
+    }
+    public String getinstructions() {
+        return instructions;
+    }
+
+    public void setactive(String instructions) {
+        this.instructions= instructions;
+    }
+    public  WorkflowStepConfig (Long id, Long templated,Integer levelNumber, String approverRole, String instructions){
+      this.id=id;
+      this.templated=templated;
+      this.levelNumber=levelNumber;
+      this.approverRole=approverRole;
+      this.instructions=instructions;
+    }
+    public  WorkflowStepConfig(){
+      
+    }
+
+}
+
