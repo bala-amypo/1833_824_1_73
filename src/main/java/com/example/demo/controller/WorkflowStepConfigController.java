aeae2533b7159a669b342f7b89f6db600b4c35c8
bala-amypo <bala@amypo.in>
2025-12-19 06:18:30 +0000

    User commit

--- src/main/java/com/example/demo/controller/WorkflowStepConfigController.java
+++ src/main/java/com/example/demo/controller/WorkflowStepConfigController.java
@@ -4,3 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public WorkflowStepConfigController{
+    
+
+}
