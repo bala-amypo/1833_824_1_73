3aa9962945517cf81ec5f519970cc6dd7e70fc9f
bala-amypo <bala@amypo.in>
2025-12-20 17:05:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ApprovalRequestService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.model.ApprovalRequest;
+import java.util.List;
+
+public interface ApprovalRequestService {
+
+    ApprovalRequest createRequest(ApprovalRequest req);
+
+    List<ApprovalRequest> getRequestsByRequester(Long userId);
+
+    List<ApprovalRequest> getAllRequests();
+}
