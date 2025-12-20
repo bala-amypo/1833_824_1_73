19f57173dabfad79c8719d1e442a58fbb45ea3d6
bala-amypo <bala@amypo.in>
2025-12-20 14:06:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/WorkflowTemplateRepository.java
+++ src/main/java/com/example/demo/repository/WorkflowTemplateRepository.java
@@ -1,12 +1,7 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import com.example.demo.model.WorkflowTemplate;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
+import com.example.demo.model.WorkflowTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// @Repository
-// public interface WorkflowTemplateRepository
-//         extends JpaRepository<WorkflowTemplate, Long> {
-
-//     boolean existsByTemplateName(String templateName);
-// }
+public interface WorkflowTemplateRepository extends JpaRepository<WorkflowTemplate, Long> {
+}
