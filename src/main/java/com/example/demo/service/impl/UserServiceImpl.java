9bb6585892541377181beb4b380cb6870889a117
bala-amypo <bala@amypo.in>
2025-12-21 18:36:20 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.model.role;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.RoleRepository;
+import org.springframework.stereotype.Service;
+import java.util.Set;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public User registerUser(User user, String roleName) {
+        role r = roleRepository.findByName(roleName);
+        user.setRoles(Set.of(r));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
