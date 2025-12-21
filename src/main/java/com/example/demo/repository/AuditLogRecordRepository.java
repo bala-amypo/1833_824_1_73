fff9ff4b944ddd57e271aeff69baacc62576ae48
bala-amypo <bala@amypo.in>
2025-12-21 17:31:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/AuditLogRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.AuditLogRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRecordRepository extends JpaRepository<AuditLogRecord, Long> {
-}
