3c50df77cd56be3d08729fdfd18e07aef7184889
bala-amypo <bala@amypo.in>
2025-12-22 09:25:46 +0000

    User commit

--- src/main/java/com/example/demo/model/WorkflowTemplate.java
+++ src/main/java/com/example/demo/model/WorkflowTemplate.java
@@ -1,67 +1,66 @@
-package com.example.demo.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
-
-
 @Entity
-public class WorkflowTemplate{
-  @Id
-  @GeneratedValue(strategy=GenerationType.IDENTITY)
-  private Long id;
-  @Column(unique=true)
-  private String templateName;
-  private String description;
-  private Integer totalLevels;
-  private Boolean active;
+public class WorkflowTemplate {
 
-  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String templateName;
+
+    private String description;
+    private Integer totalLevels;
+    private Boolean active;
+
+    public WorkflowTemplate() {
+    }
+
+    public WorkflowTemplate(Long id, String templateName, String description,
+                            Integer totalLevels, Boolean active) {
+        this.id = id;
+        this.templateName = templateName;
+        this.description = description;
+        this.totalLevels = totalLevels;
+        this.active = active;
+    }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getTemplateName() {
         return templateName;
     }
 
-    public void setTemplateName(String  templatename) {
-        this.templateName = templateName;
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName; 
     }
+
     public String getDescription() {
         return description;
     }
-    public void setDescription(String discription) {
-        this.description = description;
+
+    public void setDescription(String description) {
+        this.description = description; 
     }
+
     public Integer getTotalLevels() {
         return totalLevels;
     }
+
     public void setTotalLevels(Integer totalLevels) {
         this.totalLevels = totalLevels;
     }
+
     public Boolean getActive() {
         return active;
     }
 
     public void setActive(Boolean active) {
-        this.active= active;
+        this.active = active;
     }
-    public  WorkflowTemplate(Long id, String templateName, String description,Integer totalLevels, Boolean active){
-      this.id=id;
-      this.templateName=templateName;
-      this.description=description;
-      this.totalLevels=totalLevels;
-      this.active=active;
-
-    }
-    public  WorkflowTemplate(){
-
-    }
-
 }
-
-
