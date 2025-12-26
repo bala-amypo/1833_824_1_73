883f073e091f0049a413ab6714028ae12827abac
bala-amypo <bala@amypo.in>
2025-12-26 08:00:22 +0000

    User commit

--- src/main/java/com/example/demo/util/DataTimeUtil.java
+++ src/main/java/com/example/demo/util/DataTimeUtil.java
@@ -1,34 +1,13 @@
 package com.example.demo.util;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
 
-    private static final String DEFAULT_PATTERN =
-            "yyyy-MM-dd HH:mm:ss";
-
     private DateTimeUtil() {
-        // prevent object creation
     }
 
-    // Get current date & time
     public static LocalDateTime now() {
         return LocalDateTime.now();
-    }
-
-    // Format LocalDateTime to String
-    public static String format(LocalDateTime dateTime) {
-        return dateTime.format(
-                DateTimeFormatter.ofPattern(DEFAULT_PATTERN)
-        );
-    }
-
-    // Parse String to LocalDateTime
-    public static LocalDateTime parse(String dateTime) {
-        return LocalDateTime.parse(
-                dateTime,
-                DateTimeFormatter.ofPattern(DEFAULT_PATTERN)
-        );
     }
 }
