312016b1504074ba2b0978291e2403a36fb8f235
bala-amypo <bala@amypo.in>
2025-12-26 15:09:45 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/WorkflowStepConfigServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/WorkflowStepConfigServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.WorkflowStepConfig;
 import com.example.demo.repository.WorkflowStepConfigRepository;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -10,7 +11,8 @@ public class WorkflowStepConfigServiceImpl implements WorkflowStepConfigService 
 
     private final WorkflowStepConfigRepository workflowStepConfigRepository;
 
-    public WorkflowStepConfigServiceImpl(WorkflowStepConfigRepository workflowStepConfigRepository) {
+    public WorkflowStepConfigServiceImpl(
+            WorkflowStepConfigRepository workflowStepConfigRepository) {
         this.workflowStepConfigRepository = workflowStepConfigRepository;
     }
 
@@ -20,7 +22,7 @@ public class WorkflowStepConfigServiceImpl implements WorkflowStepConfigService 
     }
 
     @Override
-    public List<WorkflowStepConfig> getStepsForTemplate(Long templated) {
-        return workflowStepConfigRepository.findByTemplated(templated);
+    public List<WorkflowStepConfig> getStepsForTemplate(Long templateId) {
+        return workflowStepConfigRepository.findByTemplateId(templateId);
     }
 }
