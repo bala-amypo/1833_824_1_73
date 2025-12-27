9445755d1292d10e05b9fa5f215d8334409531be
bala-amypo <bala@amypo.in>
2025-12-27 05:38:54 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/WorkflowTemplateServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/WorkflowTemplateServiceImpl.java
@@ -1,51 +1,28 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.model.WorkflowTemplate;
 import com.example.demo.repository.WorkflowTemplateRepository;
+import com.example.demo.service.WorkflowTemplateService;
 import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.Optional;
 
 @Service
-public class WorkflowTemplateServiceImpl implements WorkflowTemplateService {
+public class WorkflowTemplateServiceImpl
+        implements WorkflowTemplateService {
 
     private final WorkflowTemplateRepository workflowTemplateRepository;
 
-    public WorkflowTemplateServiceImpl(WorkflowTemplateRepository workflowTemplateRepository) {
+    public WorkflowTemplateServiceImpl(
+            WorkflowTemplateRepository workflowTemplateRepository) {
         this.workflowTemplateRepository = workflowTemplateRepository;
     }
 
     @Override
-    public WorkflowTemplate createTemplate(WorkflowTemplate t) {
-        return workflowTemplateRepository.save(t);
-    }
-
-    @Override
     public WorkflowTemplate getTemplateById(Long id) {
-        return workflowTemplateRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("WorkflowTemplate not found"));
-    }
 
-    @Override
-    public List<WorkflowTemplate> getAllTemplates() {
-        return workflowTemplateRepository.findAll();
-    }
-
-    @Override
-    public WorkflowTemplate updateTemplate(Long id, WorkflowTemplate t) {
-        WorkflowTemplate existing = workflowTemplateRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("WorkflowTemplate not found"));
-
-        t.setId(existing.getId());
-        return workflowTemplateRepository.save(t);
-    }
-
-    @Override
-    public WorkflowTemplate activateTemplate(Long id, boolean active) {
-        WorkflowTemplate wt = workflowTemplateRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("WorkflowTemplate not found"));
-
-        wt.setActive(active);
-        return workflowTemplateRepository.save(wt);
+        return workflowTemplateRepository
+                .findById(id) // returns Optional<WorkflowTemplate>
+                .orElseThrow(() ->
+                        new RuntimeException("Template not found with id: " + id)
+                );
     }
 }
